5b02865dc8c3bb8f8e6a36a709cf93d46c0c0035
CLEYTON SOUZA MARTINS <24820@unifacef.edu.br>
2024-09-05 21:43:39 -0300

    First Commit

---
+++ src/main/java/br/com/unifacef/ijb/IjbApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.unifacef.ijb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IjbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IjbApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/unifacef/ijb/IjbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unifacef.ijb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IjbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
