638560a10ca694eda82a554505253016523bcea8
CleytonSM <cleytonsm1309@gmail.com>
2024-10-20 20:58:52 -0300

    feat: add CI/CD structure

--- src/test/java/br/com/unifacef/ijb/IjbApiApplicationTests.java
+++ src/test/java/br/com/unifacef/ijb/IjbApiApplicationTests.java
@@ -1,13 +1,13 @@
-package br.com.unifacef.ijb;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class IjbApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package br.com.unifacef.ijb;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class IjbApiApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
