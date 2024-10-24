e55e8cf782eaa74d0dcec2e2249bceb1d9baf055
fmatheus9 <matheus.fe.m@gmail.com>
2024-10-24 12:28:25 -0300

    Criados os DTOs e Repositories

--- src/main/java/br/com/unifacef/ijb/models/entities/User.java
+++ src/main/java/br/com/unifacef/ijb/models/entities/User.java
@@ -5,13 +5,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+
 
 
 @Data
--- src/main/java/br/com/unifacef/ijb/models/entities/Volunteer.java
+++ src/main/java/br/com/unifacef/ijb/models/entities/Volunteer.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+
 @Data
 @Entity
 @Table(name = "tb_voluntarios")
