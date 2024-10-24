e2e9bcb983f9eb4d614db2d4d831bf0051317415
fmatheus9 <matheus.fe.m@gmail.com>
2024-10-24 13:29:52 -0300

    Comentarios removidos

--- src/main/java/br/com/unifacef/ijb/models/dtos/UserDTO.java
+++ src/main/java/br/com/unifacef/ijb/models/dtos/UserDTO.java
@@ -4,8 +4,8 @@ import lombok.Data;
 
 @Data
 public class UserDTO {
-    private Integer id;          // ID do usuário
-    private String email;        // Email do usuário
-    private String cpf;          // CPF do usuário
-    private String password;     // Senha do usuário
+    private Integer id;         
+    private String email;        
+    private String cpf;          
+    private String password;     
 }
