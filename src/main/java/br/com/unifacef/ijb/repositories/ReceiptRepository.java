4e1453271268ceeffd624df97d6b1f3a283bd2f4
Gabriel Rodrigues Carbone <carbonegabriel913@gmail.com>
2024-10-25 09:55:29 -0300

    Mais uma vez corringindo erros

--- src/main/java/br/com/unifacef/ijb/repositories/ReceiptRepository.java
+++ src/main/java/br/com/unifacef/ijb/repositories/ReceiptRepository.java
@@ -1,7 +1,8 @@
 package br.com.unifacef.ijb.repositories;
 
 import br.com.unifacef.ijb.models.entities.Construction;
+import br.com.unifacef.ijb.models.entities.Receipt;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReceiptRepository extends JpaRepository<Construction, Integer> {
+public interface ReceiptRepository extends JpaRepository<Receipt, Integer> {
 }
