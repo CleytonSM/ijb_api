b46310f6fe14c20eb3ec37149ddeb7fb363ca4a3
CleytonSM <cleytonsm1309@gmail.com>
2024-10-25 08:24:33 -0300

    fix: add getters to Role Enum

--- src/main/java/br/com/unifacef/ijb/models/entities/User.java
+++ src/main/java/br/com/unifacef/ijb/models/entities/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
