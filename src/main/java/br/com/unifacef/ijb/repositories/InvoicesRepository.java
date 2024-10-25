d6f8e9352fc1958b356fc1af0d93d61996d4e383
Gabriel Rodrigues Carbone <carbonegabriel913@gmail.com>
2024-10-25 09:55:29 -0300

    Mais uma vez corringindo erros

--- src/main/java/br/com/unifacef/ijb/repositories/InvoicesRepository.java
+++ src/main/java/br/com/unifacef/ijb/repositories/InvoicesRepository.java
@@ -1,7 +1,7 @@
 package br.com.unifacef.ijb.repositories;
 
-import br.com.unifacef.ijb.models.entities.Construction;
+import br.com.unifacef.ijb.models.entities.Invoices;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface InvoicesRepository extends JpaRepository<Construction, Integer> {
+public interface InvoicesRepository extends JpaRepository<Invoices, Integer> {
 }
--- src/main/java/br/com/unifacef/ijb/repositories/MovementsTypeRepository.java
+++ src/main/java/br/com/unifacef/ijb/repositories/MovementsTypeRepository.java
@@ -1,7 +1,7 @@
 package br.com.unifacef.ijb.repositories;
 
-import br.com.unifacef.ijb.models.entities.Construction;
+import br.com.unifacef.ijb.models.entities.MovementsType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MovementsTypeRepository extends JpaRepository<Construction, Integer> {
+public interface MovementsTypeRepository extends JpaRepository<MovementsType, Integer> {
 }
