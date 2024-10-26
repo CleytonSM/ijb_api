6e4b5befb9021391ebe65009ef9d9718e899bc9a
Gabriel Rodrigues Carbone <carbonegabriel913@gmail.com>
2024-10-26 12:23:51 -0300

    Adicionando a anotação Repository

--- src/main/java/br/com/unifacef/ijb/repositories/ReceiptRepository.java
+++ src/main/java/br/com/unifacef/ijb/repositories/ReceiptRepository.java
@@ -1,8 +1,9 @@
 package br.com.unifacef.ijb.repositories;
 
-import br.com.unifacef.ijb.models.entities.Construction;
 import br.com.unifacef.ijb.models.entities.Receipt;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReceiptRepository extends JpaRepository<Receipt, Integer> {
 }
