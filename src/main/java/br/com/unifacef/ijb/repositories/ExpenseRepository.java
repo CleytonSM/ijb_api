63580ee8cd07c0337a026178729a5670c9566adb
Gabriel Rodrigues Carbone <carbonegabriel913@gmail.com>
2024-10-26 12:39:01 -0300

    Adicionando a anotaÃção Repository

--- src/main/java/br/com/unifacef/ijb/repositories/ExpenseRepository.java
+++ src/main/java/br/com/unifacef/ijb/repositories/ExpenseRepository.java
@@ -4,6 +4,7 @@ import br.com.unifacef.ijb.models.entities.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Integer> {
 }
--- src/main/java/br/com/unifacef/ijb/repositories/MovementRepository.java
+++ src/main/java/br/com/unifacef/ijb/repositories/MovementRepository.java
@@ -4,6 +4,7 @@ import br.com.unifacef.ijb.models.entities.Movement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface MovementRepository extends JpaRepository<Movement, Integer> {
 }
