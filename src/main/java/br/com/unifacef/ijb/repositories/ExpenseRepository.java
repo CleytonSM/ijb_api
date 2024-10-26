5589e7ece74e29151da7dc1b8154f38c95babaaf
Gabriel Rodrigues Carbone <carbonegabriel913@gmail.com>
2024-10-26 13:49:44 -0300

    Atualizando novamente

--- src/main/java/br/com/unifacef/ijb/repositories/ExpenseRepository.java
+++ src/main/java/br/com/unifacef/ijb/repositories/ExpenseRepository.java
@@ -4,7 +4,6 @@ import br.com.unifacef.ijb.models.entities.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Integer> {
 }
--- src/main/java/br/com/unifacef/ijb/repositories/ReceiptRepository.java
+++ src/main/java/br/com/unifacef/ijb/repositories/ReceiptRepository.java
@@ -4,7 +4,6 @@ import br.com.unifacef.ijb.models.entities.Receipt;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public interface ReceiptRepository extends JpaRepository<Receipt, Integer> {
 }
