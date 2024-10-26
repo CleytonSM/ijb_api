974e4360ad7106ba7dc463ca7f2708aec375a3f6
Gabriel Rodrigues Carbone <carbonegabriel913@gmail.com>
2024-10-26 12:39:01 -0300

    Adicionando a anotaÃção Repository

--- src/main/java/br/com/unifacef/ijb/repositories/MovementsTypeRepository.java
+++ src/main/java/br/com/unifacef/ijb/repositories/MovementsTypeRepository.java
@@ -4,6 +4,7 @@ import br.com.unifacef.ijb.models.entities.MovementsType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface MovementsTypeRepository extends JpaRepository<MovementsType, Integer> {
 }
