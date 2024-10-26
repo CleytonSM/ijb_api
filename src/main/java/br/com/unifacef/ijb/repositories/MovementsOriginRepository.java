3f6f6f10648ab903d044ca33b7ef270c70ecbd95
Gabriel Rodrigues Carbone <carbonegabriel913@gmail.com>
2024-10-26 13:49:44 -0300

    Atualizando novamente

--- src/main/java/br/com/unifacef/ijb/repositories/MovementsOriginRepository.java
+++ src/main/java/br/com/unifacef/ijb/repositories/MovementsOriginRepository.java
@@ -4,7 +4,6 @@ import br.com.unifacef.ijb.models.entities.MovementsOrigin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public interface MovementsOriginRepository extends JpaRepository<MovementsOrigin, Integer> {
 }
--- src/main/java/br/com/unifacef/ijb/repositories/MovementsSituationRepository.java
+++ src/main/java/br/com/unifacef/ijb/repositories/MovementsSituationRepository.java
@@ -4,7 +4,6 @@ import br.com.unifacef.ijb.models.entities.MovementsSituation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public interface MovementsSituationRepository extends JpaRepository<MovementsSituation, Integer> {
 }
