4836649c2413ea486f678bb060e202eed7824ac8
CleytonSM <cleytonsm1309@gmail.com>
2024-10-27 15:24:49 -0300

    feat: finish basic crud methods

--- src/main/java/br/com/unifacef/ijb/services/OutletProductService.java
+++ src/main/java/br/com/unifacef/ijb/services/OutletProductService.java
@@ -76,4 +76,20 @@ public class OutletProductService {
 
         return outletProduct;
     }
+
+    @Transactional
+    public void deleteOutletProduct(Integer id) {
+        OutletProduct outletProduct = OptionalHelper.getOptionalEntity(repository.findById(id));
+        save(changeOutletProductStatus(OutletProductStatus.INACTIVE, outletProduct));
+    }
+
+    private void updateRetrievedEntity(OutletProductDTO outletProductUpdate, OutletProduct outletProduct) {
+        OutletProductMapper.updateOutletProduct(outletProductUpdate, outletProduct);
+    }
+
+    private OutletProduct changeOutletProductStatus(OutletProductStatus status, OutletProduct outletProduct) {
+        outletProduct.setStatus(status);
+
+        return outletProduct;
+    }
 }
