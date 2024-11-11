ecf35ce814b46285d890fcf9b16e9541bde23c4b
Lucas Vizoto <lucasvizoto364@gmail.com>
2024-11-11 19:29:07 -0300

    making VisitServices

--- src/main/java/br/com/unifacef/ijb/models/entities/Visit.java
+++ src/main/java/br/com/unifacef/ijb/models/entities/Visit.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
+import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
