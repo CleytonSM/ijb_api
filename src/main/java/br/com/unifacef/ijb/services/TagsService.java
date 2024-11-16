c2a6d78ed2f7af1437e3ea73f04f62f8bac76233
PGalmeida <pedro8tp@gmail.com>
2024-11-16 19:54:48 -0300

    marketing - update

--- src/main/java/br/com/unifacef/ijb/services/TagsService.java
+++ src/main/java/br/com/unifacef/ijb/services/TagsService.java
@@ -1,55 +1,64 @@
 package br.com.unifacef.ijb.services;
 
 import br.com.unifacef.ijb.helpers.OptionalHelper;
+import br.com.unifacef.ijb.mappers.PurchasedMaterialMapper;
 import br.com.unifacef.ijb.mappers.TagsMapper;
+import br.com.unifacef.ijb.models.dtos.PurchasedMaterialCreateDTO;
+import br.com.unifacef.ijb.models.dtos.PurchasedMaterialDTO;
+import br.com.unifacef.ijb.models.dtos.TagsCreateDTO;
 import br.com.unifacef.ijb.models.dtos.TagsDTO;
+import br.com.unifacef.ijb.models.entities.DonatedMaterial;
+import br.com.unifacef.ijb.models.entities.PurchasedMaterial;
 import br.com.unifacef.ijb.models.entities.Tags;
 import br.com.unifacef.ijb.repositories.TagsRepository;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-    @Service
-    public class TagsService {
-        @Autowired
-        private TagsRepository tagsRepository;
+@Service
+public class TagsService {
+    @Autowired
+    private TagsRepository tagsRepository;
 
-        public Tags save(Tags tags) {
-            return tagsRepository.save(tags);
-        }
-
-        public Tags getById(Integer id) {
-            return OptionalHelper.getOptionalEntity(tagsRepository.findById(id));
-        }
-
-        @Transactional
-        public TagsDTO createTags(TagsDTO tags) {
-            Tags tags = TagsMapper.convertTagsDTOIntoTags(tags);
-
-            return TagsMapper.convertTagsIntoTagsDTO(save(tags));
-        }
-
-        public List<TagsDTO> getAllTags() {
-            return TagsMapper.convertListOfTagsIntoListOfTagsDTO(tagsRepository.findAll());
-        }
-
-        @Transactional
-        public TagsDTO updateTags(TagsDTO tagsUpdate) {
-            tags tags = getById(TagsUpdate.getId());
-            updateRetrievedEntity(tagsUpdate, tags);
-
-            return TagsMapper.convertTagsIntoTagsDTO(save(tags));
-        }
-
-        @Transactional
-        public void deleteTags(Integr id) {
-            if (tagsRepository.existsById(id)) {
-                tagsRepository.deleteById(id);
-            } else {
-                throw new IllegalArgumentException("Tag não encontrada");
-            }
-        }
+    public Tags save(Tags tags) {
+        return tagsRepository.save(tags);
     }
+
+    public Tags getById(Integer id) {
+        return OptionalHelper.getOptionalEntity(tagsRepository.findById(id));
+    }
+
+    @Transactional
+    public TagsDTO createTags(TagsCreateDTO tagsCreate) {
+        Tags tags = TagsMapper
+                .convertTagsCreateDTOIntoTags(tagsCreate);
+
+        return TagsMapper.convertTagsIntoTagsDTO(save(tags));
+    }
+
+    public List<TagsDTO> getAllTags() {
+        return TagsMapper.convertListOfTagsIntoListOfTagsDTO(tagsRepository.findAll());
+    }
+
+    @Transactional
+    public TagsDTO updateTags(TagsDTO tagsUpdate) {
+        Tags tags = getById(tagsUpdate.getId());
+        tagsUpdate(tagsUpdate, tags);
+
+        return TagsMapper.convertTagsIntoTagsDTO(save(tags));
+    }
+
+    @Transactional
+    public void deleteTags(Integer id) {
+        Tags tags = OptionalHelper.getOptionalEntity(tagsRepository.findById(id));
+        tagsRepository.delete(tags);
+    }
+
+    private void tagsUpdate(TagsDTO tagsUpdate, Tags tags) {
+        TagsMapper.updateTags(tagsUpdate, tags);
+    }
+}
 
