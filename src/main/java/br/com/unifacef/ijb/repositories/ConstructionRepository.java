596bfdf718d0523941c06af424371e95f55d07eb
Cleyton Souza Martins <122110138+CleytonSM@users.noreply.github.com>
2024-11-20 19:32:55 -0300

    Revert "Feature/obras"

--- src/main/java/br/com/unifacef/ijb/repositories/ConstructionRepository.java
+++ src/main/java/br/com/unifacef/ijb/repositories/ConstructionRepository.java
@@ -1,14 +1,9 @@
 package br.com.unifacef.ijb.repositories;
 
 import br.com.unifacef.ijb.models.entities.Construction;
-import br.com.unifacef.ijb.models.entities.OutletProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ConstructionRepository extends JpaRepository<Construction, Integer> {
-
-    List<Construction> findAllByConstructionDescription(String constructionDescription);
 }
