7c6be20eba903db86ac6c603c3c093e624668fb6
CleytonSM <cleytonsm1309@gmail.com>
2024-11-23 14:41:39 -0300

    add necessary insert in migration

--- src/main/resources/db.migration/V5__VolunteerType_insert.sql
+++ src/main/resources/db.migration/V5__VolunteerType_insert.sql
@@ -1,2 +1,5 @@
 INSERT INTO tb_tipos_voluntarios(nm_tipo_voluntario, dt_criacao, dt_alteracao, dt_exclusao)
 VALUES ("VOLUNTARIO_APOIADOR", NOW(), NOW(), NULL);
+
+INSERT INTO tb_tipos_voluntarios(nm_tipo_voluntario, dt_criacao, dt_alteracao, dt_exclusao)
+VALUES ("VOLUNTARIO", NOW(), NOW(), NULL);
