73d09f70313c1b8ba0336608089e1f5fb8ce9181
CleytonSM <cleytonsm1309@gmail.com>
2024-11-23 14:56:23 -0300

    add rebase

--- src/main/java/br/com/unifacef/ijb/repositories/VolunteerRepository.java
+++
@@ -1,6 +0,0 @@
-package br.com.unifacef.ijb.repositories;
-
-import br.com.unifacef.ijb.models.entities.Volunteer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VolunteerRepository extends JpaRepository<Volunteer, Long> {}
