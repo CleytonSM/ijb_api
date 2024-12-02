5b8399cd6a18ec17011bf93d138a134402a90600
fmatheus9 <matheus.fe.m@gmail.com>
2024-12-02 17:28:22 -0300

    Update

--- src/main/java/br/com/unifacef/ijb/controller/EmailController.java
+++ src/main/java/br/com/unifacef/ijb/controller/EmailController.java
@@ -2,7 +2,10 @@ package br.com.unifacef.ijb.controller;
 
 import br.com.unifacef.ijb.services.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,13 +14,50 @@ public class EmailController {
     @Autowired
     private EmailService emailService;
 
-    @GetMapping("/enviar-email")
-    public String enviarEmail() {
-        emailService.enviarEmail(
-                "matheusspasti@gmail.com",
-                "assunto teste",
-                "teste"
-        );
-        return "Email enviado com sucesso!";
-}
+    public static class EmailRequest {
+        private String to;
+        private String subject;
+        private String body;
+
+        public String getTo() {
+            return to;
+        }
+
+        public void setTo(String to) {
+            this.to = to;
+        }
+
+        public String getSubject() {
+            return subject;
+        }
+
+        public void setSubject(String subject) {
+            this.subject = subject;
+        }
+
+        public String getBody() {
+            return body;
+        }
+
+        public void setBody(String body) {
+            this.body = body;
+        }
+    }
+
+
+    @GetMapping("/send-email")
+    public ResponseEntity<String> enviarEmail(@RequestBody EmailRequest emailRequest) {
+        try {
+            emailService.enviarEmail(
+                    emailRequest.getTo(),
+                    emailRequest.getSubject(),
+                    emailRequest.getBody()
+            );
+            return ResponseEntity.ok("Email enviado com sucesso!");
+        } catch (MailException e) {
+            return ResponseEntity.status(500).body("Erro ao enviar o e-mail: " + e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Ocorreu um erro inesperado: " + e.getMessage());
+        }
+    }
 }
