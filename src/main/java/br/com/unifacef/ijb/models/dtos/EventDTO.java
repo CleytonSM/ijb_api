1edceb6ac744410869d07a3cba61b10be3fd5e86
João Vitor Borges <80920809+Troves@users.noreply.github.com>
2024-12-04 19:12:42 -0300

    Update EventDTO.java

--- src/main/java/br/com/unifacef/ijb/models/dtos/EventDTO.java
+++ src/main/java/br/com/unifacef/ijb/models/dtos/EventDTO.java
@@ -1,5 +1,8 @@
 package br.com.unifacef.ijb.models.dtos;
 
+import lombok.Data;
+
+@Data
 public class EventDTO {
 
     private Long id;
@@ -9,21 +12,5 @@ public class EventDTO {
     private String state;
     private String code;
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
-
-    public String getDate() { return date; }
-    public void setDate(String date) { this.date = date; }
-
-    public String getState() { return state; }
-    public void setState(String state) { this.state = state; }
-
-    public String getCode() { return code; }
-    public void setCode(String code) { this.code = code; }
 }
+
