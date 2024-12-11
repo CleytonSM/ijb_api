bd8d3eefba8c965cbe730d3cbad6c960a8185bf7
João Vitor Borges <80920809+Troves@users.noreply.github.com>
2024-12-10 22:31:26 -0300

    Create EventDTO.java

---
+++ src/main/java/br/com/unifacef/ijb/models/dtos/EventDTO.java
@@ -0,0 +1,15 @@
+package br.com.unifacef.ijb.models.dtos;
+
+import lombok.Data;
+
+@Data
+public class EventDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private String date;
+    private String state;
+    private String code;
+
+}
